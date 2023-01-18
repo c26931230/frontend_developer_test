44a3a6c0f4f62ae60d8e4aa9c7ddab2b66ab65bb
Mandy_Li <c26931230@gmail.com>
2023-01-18 13:11:43 +0800

    deploy setting

--- vue.config.js
+++ vue.config.js
@@ -9,20 +9,8 @@ module.exports = defineConfig({
 		},
 	},
 	publicPath: process.env.NODE_ENV === 'production'
-		? '/cgd103/g2/back/'
+		? '/frontend_developer_test/'
 		: '/',
-	devServer: {
-		proxy: {
-			'/api_server': {
-				target: 'http://localhost/cgd103_g2_backend/phpfile',//本地
-				// target: 'https://tibamef2e.com/cgd103/g2/back/phpfile',//上線
-				changeOrigin: true,
-				pathRewrite: {
-					'^/api_server': ''
-				}
-			}
-		}
-	},
 	productionSourceMap: false,
 	filenameHashing: false
 })
