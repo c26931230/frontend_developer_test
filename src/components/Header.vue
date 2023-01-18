6e6b8b09018f7968aafd144e08483043e465a2f3
Mandy_Li <c26931230@gmail.com>
2023-01-18 17:16:17 +0800

    調max-width和優化css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,15 +3,17 @@
         <div class="holder" ref="holder" :style="{
             backgroundColor: $store.state.isActive ? 'white' : 'transparent'
         }">
-            <a href="#" class="logo">
-                <img src="@/assets/img/logo_blue.png" alt="digisalad logo" v-show="$store.state.isActive == true">
-            </a>
-            <div class="function_box">
-                <a href="#" class="btn_1">START YOUR PROJECT</a>
-                <div class="toggle">
-                    <span v-for="i in 3" :style="{
-                        backgroundColor: $store.state.isActive ? '#585880' : 'white'
-                    }"></span>
+            <div class="wrapper">
+                <a href="#" class="logo">
+                    <img src="@/assets/img/logo_blue.png" alt="digisalad logo" v-show="$store.state.isActive == true">
+                </a>
+                <div class="function_box">
+                    <a href="#" class="btn_1">START YOUR PROJECT</a>
+                    <div class="toggle">
+                        <span v-for="i in 3" :style="{
+                            backgroundColor: $store.state.isActive ? '#585880' : 'white'
+                        }"></span>
+                    </div>
                 </div>
             </div>
 
